fd09f1c5ddeac362380e26b06cc16047688e3e7e
TarunVallabhuni <cunningjack@gmail.com>
2020-03-06 14:15:27 +0100

    Update Article_Sentiment.py

--- Article_Sentiment.py
+++ Article_Sentiment.py
@@ -1,9 +1,6 @@
-
 #Get the sentiment of articles from the internet
 
 #pip install newspaper3k
-#pip install textblob
-#pip install nltk
 
 #Import the libraries
 from textblob import TextBlob
@@ -20,14 +17,11 @@ article.parse() #Parse the article
 nltk.download('punkt')#1 time download of the sentence tokenizer
 article.nlp()#  Keyword extraction wrapper
 
-#Get the summary of the article
 text = article.summary
 
 #print text
 print(text)
 
-#Create Text Blob Object
-#NOTE: You can treat TextBlob objects as if they were Python strings that learned how to do Natural Language Processing.
 obj = TextBlob(text)
 
 #returns the sentiment of text
@@ -35,10 +29,9 @@ obj = TextBlob(text)
 sentiment = obj.sentiment.polarity
 print(sentiment)
 
-#Print if the article was neutral, positive, or negative
 if sentiment == 0:
-  print('The text is neutral')
+  print('The article is neutral')
 elif sentiment > 0:
-  print('The text is positive')
+  print('The article is positive')
 else:
-  print('The text is negative')
+  print('The article is negative')
